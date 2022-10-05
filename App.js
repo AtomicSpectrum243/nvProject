755b630eda40d59f79df91d2b5cc360a5ee1cfb4
AtomicSpectrum243 <7523494@gmail.com>
2022-10-05 11:01:42 +0700

    new commit

--- App.js
+++ App.js
@@ -1,54 +1,86 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View, Button, TextInput, StyleSheet, Image } from "react-native";
-
-import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as React from "react";
+import { Button, View, Text ,SafeAreaView,Image,StyleSheet} from "react-native";
 import {
   createDrawerNavigator,
   DrawerContentScrollView,
   DrawerItemList,
   DrawerItem,
 } from "@react-navigation/drawer";
-import {
-  HeaderButtons,
-  HeaderButton,
-  Item,
-  HiddenItem,
-  OverflowMenu,
-} from "react-navigation-header-buttons";
-
-import HomeScreen from "./screens/HomeScreen";
-import ProductScreen from "./screens/ProductScreen";
-
-import React from "react";
-import { SafeAreaView } from "react-native-web";
-
-const myTheme = {
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import{createNativeStackNavigator} from '@react-navigation/native-stack'
+import HomeScreen from "./screens/HomeScreen"
+import DetailScreen from "./screens/DetailScreen"
+import ProductScreen from "./screens/ProductScreen"
+const MyTheme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
     primary: "rgb(255,45,85)",
+    // background:'rgb(242,242,242)',
+    // card:'rgb(255,255,255)',
+    // text:'rgb(28,28,30)',
+    // border:'rgb(199,199,204)',
+    // notification:'rgb(255,69,58)',
   },
 };
-
-function CustomDrawerContent(props) {
+function Feed({ navigation }) {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Image
-        style={styles.sideMenuProfileIcon}
-        source={require("./assets/react_logo.png")}
-      />
-      <DrawerContentScrollView {...props}>
-        <DrawerItemList {...props} />
-        <DrawerItem
-          label="close Drawer"
-          onPress={() => props.navigation.closeDrawer()}
-        />
-      </DrawerContentScrollView>
-    </SafeAreaView>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      {/* <Button
+        onPress={() => navigation.navigate("Notifications")}
+        title="Go to notifications"
+      /> */}
+      <Text>Feed Screen</Text>
+      <Button onPress={() => navigation.openDrawer()} title="Open Drawer" />
+    </View>
   );
 }
 
+// function Product({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+//       {/* <Button onPress={() => navigation.goBack()} title="Go back home" /> */}
+//       <Text>สินค้า</Text>
+      
+//     </View>
+//   );
+// }
+
+function CustomDrawerContent(props) {
+  return (
+    <SafeAreaView style={styles.container}>
+      
+    <DrawerContentScrollView {...props}>
+    <Image source={require('./assets/react_logo.png')} style={styles.sideMenuProfileIcon}/>
+      <DrawerItemList {...props} />
+      <DrawerItem
+        label="Close Drawer"
+        onPress={() => props.navigation.closeDrawer()}
+      />
+    </DrawerContentScrollView>
+    </SafeAreaView>
+  );
+}
+const Stack = createNativeStackNavigator();
+function ProductStack(){
+  return(
+    <Stack.Navigator
+    screenOptions = {{
+      headerStyle:{
+        backgroundColor:'green'
+      },
+      headerTintColor:'white',
+      headerTitleStyle:{
+        fontWeight:'bold'
+      }
+    }}
+    >
+      <Stack.Screen name="Product" component = {ProductScreen}/>
+      <Stack.Screen name="Detail" component = {DetailScreen}/>
+    </Stack.Navigator>
+
+  )
+}
 const Drawer = createDrawerNavigator();
 
 function MyDrawer() {
@@ -58,32 +90,31 @@ function MyDrawer() {
       drawerContent={(props) => <CustomDrawerContent {...props} />}
       screenOptions={{
         drawerStyle: {
+          // backgroundColor:'cyan',
           width: 240,
         },
       }}
     >
+      
       <Drawer.Screen name="Home" component={HomeScreen} />
-      <Drawer.Screen name="Product" component={ProductScreen} />
+      <Drawer.Screen name="Product" component={ProductStack} />
     </Drawer.Navigator>
   );
 }
 
-const App = () => {
+export default function App() {
   return (
-    <NavigationContainer theme={myTheme}>
+    <NavigationContainer theme={MyTheme}>
       <MyDrawer />
     </NavigationContainer>
   );
-};
-
-export default App;
-
+}
 const styles = StyleSheet.create({
   sideMenuProfileIcon: {
     resizeMode: "center",
     width: 100,
     height: 100,
-    borderRadius: 100/2,
+    borderRadius: 100 / 2,
     alignSelf: "center",
   },
 });
--- Screens/ProductScreen.js
+++ Screens/ProductScreen.js
@@ -1,88 +1,162 @@
 import {
-    StyleSheet,
-    Text,
-    View,
-    ActivityIndicator,
-    FlatList,
-    Image,
-  } from "react-native";
-  import React, { useEffect, useState } from "react";
-  
-  import axios from "axios";
-  
-  const ProductScreen = () => {
-    const [product, setProduct] = useState([]);
-  
-    const getData = async () => {
-      const res = await axios.get("https://api.codingthailand.com/api/course");
-      console.log(res.data.data);
-      setProduct(res.data.data);
-    };
-  
-    useEffect(() => {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  FlatList,
+  Image,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native"; //
+import React, { useState, useEffect, useCallback } from "react";
+import { useFocusEffect } from "@react-navigation/native";
+import axios from "axios";
+
+const ProductScreen = ({navigation}) => {
+  const [product, setProduct] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useFocusEffect(
+    useCallback(() => {
       getData();
-    }, []);
-  
-    const _renderItem = ({ item }) => {
-      return (
-        <View style={styles.dataContainer}>
-          <View style={styles.container}>
-            <Image
-              style={styles.thumbnail}
-              source={{ uri: item.picture }}
-              resizeMode="cover"
-            />
-  
-            <View style={styles.dataContent}>
-              <Text style={styles.title}>{item.title}</Text>
-              <Text style={styles.detail}>{item.detail}</Text>
-            </View>
-          </View>
-        </View>
-      );
-    };
-  
+    }, [])
+  );
+  // useEffect(()=>{
+
+  //     getData();
+  // },[])
+
+  if (error) {
+    //ถ้าerror จะreturn UI กลับไป
     return (
-      <View>
-        <FlatList
-          data={product}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={_renderItem}
-        />
+      <View style={styles.container}>
+        <Text>{error.message}</Text>
       </View>
     );
+  }
+
+  if (loading === true) {
+    return (
+      <View>
+        <ActivityIndicator color="green" size="large" />
+      </View>
+    );
+  }
+
+  const _onRefresh = () => {
+    getData();
   };
-  
-  export default ProductScreen;
-  
-  const styles = StyleSheet.create({
-    container: {
-      height: 80,
-      elevation: 3,
-      borderColor: "gray",
-      borderRadius: 5,
-      flexDirection: "row",
-      marginHorizontal: 20,
-    },
-    dataContainer: {
-      flex: 1,
-    },
-    thumbnail: {
-      width: 70,
-      height: 70,
-    },
-    dataContent: {
-      marginTop: 5,
-      marginLeft: 15,
-    },
-    title: {
-      color: "#444",
-      fontSize: 18,
-      fontWeight: "bold",
-    },
-    detail: {
-      fontSize: 16,
-      color: "#888",
-      fontWeight: "700",
-    },
-  });
+
+  const getData = async () => {
+    try {
+      setLoading(true);
+      const res = await axios.get("https://api.codingthailand.com/api/course");
+      //console.log(res.data.data)
+      setProduct(res.data.data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setError(error); //set error to state of error ว่าเกิดอะไรขึ้น
+    }
+  };
+
+  const ItemSeparatorView = () => {
+    return (
+      // Flat List Item Separator
+      <View
+        style={{
+          height: 0.5,
+          width: "100%",
+          backgroundColor: "gray",
+        }}
+      />
+    );
+  };
+
+  const _renderItem = ({ item }) => {
+    let picture = (item.picture!==null)
+      ?item.picture
+      :'https://via.placeholder.com/150';
+    return (
+      <SafeAreaView>
+        <TouchableOpacity style ={styles.addButtonStyle}
+          onPress={()=>{
+            navigation.navigate('Detail',{
+              id:item.id,
+              title:item.title
+            })
+
+          }}
+        >
+          <View style={styles.dataContainer}>
+            <View style={styles.container}>
+              <Image
+                resizeMode="cover"
+                source={{ uri: picture }}
+                style={styles.thumbnail}
+              />
+              <View style={styles.dataContent}>
+                <Text style={styles.title}>{item.title}</Text>
+                <Text style={styles.detail}>{item.detail}</Text>
+              </View>
+            </View>
+          </View>
+        </TouchableOpacity>
+      </SafeAreaView>
+    );
+  };
+  return (
+    <View>
+      <FlatList
+        data={product}
+        keyExtrator={(item, index) => item.id.toString()}
+        ItemSeparatorComponent={ItemSeparatorView}
+        renderItem={_renderItem}
+        refreshing={loading}
+        _onRefresh={_onRefresh}
+      />
+    </View>
+  );
+};
+
+export default ProductScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    height: 80,
+    elevation: 3,
+    borderColor: "gray",
+    borderRadius: 5,
+    flexDirection: "row",
+    marginHorizontal: 20,
+  },
+  dataContainer: {
+    flex: 1,
+  },
+  thumbnail: {
+    width: 70,
+    height: 70,
+  },
+  dataContent: {
+    marginTop: 5,
+    marginLeft: 15,
+  },
+  title: {
+    color: "#444",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  detail: {
+    fontSize: 16,
+    color: "#888",
+    fontWeight: "700",
+  },
+  addButtonStyle: {
+
+    width: '100%',         
+
+    marginBottom: 15,
+
+},
+});
